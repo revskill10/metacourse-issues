55b1da96cf28b428b221f597fdd4750596b98e4c
Nguyễn Trần Anh Duy <84486806+Anhduy-git@users.noreply.github.com>
2022-10-08 20:32:09 +0700

    add client file

--- software-engineering/services/main-service/main.js
+++ software-engineering/services/main-service/main.js
@@ -1,11 +1,12 @@
 const express = require('express');
-const axios = require('axios');
 const app = express();
 const PORT = 3001;
-const { InternalServerError } = require('../errors/custom-error');
-const errorHandler = require('../errors/error-handler');
+const errorHandler = require('../../errors/error-handler');
+const { MathServiceClient } = require('./client');
+
 
 app.use(express.json());
+
 
 //api
 app.post('/api/math', async (req, res, next) => {
@@ -13,12 +14,13 @@ app.post('/api/math', async (req, res, next) => {
 		const { op, parameters } = req.body;
 		if (op === 'sum') {
 			const x = parameters[0];
-			const y = parameters[1];			
-			const response = await axios.post('http://localhost:3000/api/sum', {x, y});
-			const result = response.data.result;
+			const y = parameters[1];
+			const mathServiceClient = new MathServiceClient();
+			const response = await mathServiceClient.sum({ x, y });
+			const result = response.result;
 			res.json({ result });
 		}
-	} catch(err) {
+	} catch (err) {
 		next(err);
 	}
 });
--- software-engineering/services/math-service/math.js
+++ software-engineering/services/math-service/math.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const app = express();
-const PORT = 3000;
+const PORT = 4000;
 
 app.use(express.json());
 
 app.post('/api/sum', (req, res) => {
-	const {x, y} = req.body;
+	const { x, y } = req.body;
 	res.json({
 		result: x + y
 	})
